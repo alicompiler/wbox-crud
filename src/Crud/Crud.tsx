2807aa990ff648b5648bddd1ada63b6f90b17002
Ali Faris <alihcompiler@gmail.com>
2022-08-27 23:28:21 +0300

    minor cleanups

--- src/Crud/Crud.tsx
+++ src/Crud/Crud.tsx
@@ -4,19 +4,21 @@ import {CoreProvider} from "wbox-context";
 import {modalReducer} from "../Data/Reducers/ModalReducer";
 import {CrudProps} from "./CrudProps";
 import {useDefaults} from "../Defaults/DefaultsContext";
-
+import {ModulesProvider} from "../Modules/ModulesProvider";
 
 export function Crud(props: CrudProps) {
     const defaults = useDefaults();
     const routesWrapper = props.routesWrapper ?? defaults.routesWrapper;
     const modalWrapper = props.modalWrapper ?? defaults.modalWrapper;
-    return <BrowserRouter>
-        <CoreProvider createServiceFactory={() => null}
-                      reducers={[modalReducer]}
-                      initialState={{modal: undefined}}>
-            <h1>wbox-crud</h1>
-            {routesWrapper(props)}
-            {modalWrapper(props)}
-        </CoreProvider>
-    </BrowserRouter>
+    return <ModulesProvider value={props.modules}>
+        <BrowserRouter>
+            <CoreProvider createServiceFactory={() => null}
+                          reducers={[modalReducer]}
+                          initialState={{modal: {open: false, moduleName: undefined}}}>
+                <h1>wbox-crud</h1>
+                {routesWrapper(props)}
+                {modalWrapper(props)}
+            </CoreProvider>
+        </BrowserRouter>
+    </ModulesProvider>
 }
--- src/Data/State.ts
+++ src/Data/State.ts
@@ -1,5 +1,6 @@
-import {ModuleConfiguration} from "../Modules/ModuleConfiguration";
-
 export interface State {
-    modal?: ModuleConfiguration;
+    modal: {
+        open: boolean;
+        moduleName?: string;
+    };
 }
